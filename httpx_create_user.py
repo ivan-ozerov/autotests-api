3eeb3eb622c842cd142580311f479d6133bf5086
ozerov-ivan <eto.denb.takoi@gmail.com>
2026-01-18 16:30:29 +0300

    adding user get, create, delete scripts

---
+++ httpx_create_user.py
@@ -0,0 +1,13 @@
+import httpx
+from tools.fakers import get_random_email
+
+create_user_payload = {
+    "email": get_random_email(),   
+    "password": "string",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string"
+}
+create_user_response = httpx.post("http://localhost:8000/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+print('Create user data:', create_user_response_data)
