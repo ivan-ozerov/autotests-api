40d786e3bd7aebf2d6220f6fed3d430467bd0875
ozerov-ivan <eto.denb.takoi@gmail.com>
2026-01-20 22:24:23 +0300

    adding api_client and auth client

---
+++ clients/api_client.py
@@ -0,0 +1,47 @@
+from typing import Any
+from httpx import URL, Client, QueryParams, Response
+from httpx._types import RequestData, RequestFiles
+
+class APIClient:
+    def __init__(self, client: Client):
+        """
+        Базовый API клиент, принимающий объект httpx.Client.
+        
+        :param client: экземпляр httpx.Client для выполнения HTTP-запросов
+        """
+        self.client = client
+
+    def get(self, url: URL | str, params: QueryParams | None = None) -> Response:
+        """
+        Выполняет GET-запрос.
+        
+        :param url: URL-адрес эндпоинта.
+        :param params: GET-параметры запроса (например, ?key=value).
+        :return: Объект Response с данными ответа.
+        """
+        return self.client.get(url, params=params)
+    
+    def post(
+            self,
+            url: URL | str,
+            json: Any | None = None,
+            data: RequestData | None = None,
+            files: RequestFiles | None = None
+    ) -> Response:
+        """
+        Выполняет POST-запрос.
+        
+        :param url: URL-адрес эндпоинта.
+        :param json: Данные в формате JSON.
+        :param data: Форматированные данные формы (например, application/x-www-form-urlencoded).
+        :param files: Файлы для загрузки на сервер.
+        :return: Объект Response с данными ответа.
+        """
+        return self.client.post(url, json=json, data=data, files=files)
+    
+    def patch(self, url: URL | str, json: Any | None = None) -> Response:
+        return self.client.patch(url, json=json)
+    
+    def delete(self, url: URL | str) -> Response:
+        return self.client.delete(url)
+    
