ac68667474e9247f61c04071fffe482f11578356
ozerov-ivan <eto.denb.takoi@gmail.com>
2026-01-20 22:25:22 +0300

    Знакомство с API клиентами. Практика: Написание API клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,27 @@
+from typing import TypedDict
+from httpx import Response
+from clients.api_client import APIClient
+
+
+class CreateUserRequestDict(TypedDict):
+    """
+    Описание структуры запроса для создания пользователя
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для публичных запросов к /api/v1/users
+    """
+    def create_user_api(self, request: CreateUserRequestDict) -> Response:
+        """
+        Метод создает пользователя
+        
+        :param request: Словарь с email, password, lastName, firstName, middleName
+        :return: ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post(url="/api/v1/users", json=request)
